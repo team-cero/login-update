677bdd31da101b5f5d1e855a5bb79eeb0a3c804f
naodps <nao.yamato@dpschool.io>
2020-03-21 23:36:12 +0900

    added introduction img

--- app/src/main/java/com/example/culctogoalset/Sample1Fragment.kt
+++ app/src/main/java/com/example/culctogoalset/Sample1Fragment.kt
@@ -1,14 +1,11 @@
 package com.example.culctogoalset
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 
 class Sample1Fragment : Fragment() {
 
@@ -19,5 +16,6 @@ class Sample1Fragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sample1, container, false)
     }
+
 
 }
--- app/src/main/java/com/example/culctogoalset/Timeline.kt
+++ app/src/main/java/com/example/culctogoalset/Timeline.kt
@@ -1,11 +1,13 @@
 package com.example.culctogoalset
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_timeline.*
 
 
 class Timeline : AppCompatActivity() {
@@ -15,6 +17,30 @@ class Timeline : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline)
 
+        //game challenge
+
+        //calculator
+        var btnCalc = findViewById<Button>(R.id.btnCalc)
+        btnCalc.setOnClickListener {
+            val intent = Intent(application, CalculationActivity::class.java)
+            startActivity(intent)
+        }
+
+        //goal setting
+        var btnGoal= findViewById<Button>(R.id.btnGoal)
+        btnGoal.setOnClickListener {
+            val intent2 = Intent(application, GoalSettingActivity::class.java)
+            startActivity(intent2)
+        }
+
+        //record
+        var btnReacord= findViewById<Button>(R.id.btnRecord)
+        btnGoal.setOnClickListener {
+            //val intent = Intent(application, RecordActivity::class.java)
+            //startActivity(intent)
+        }
+
+         //logout
         var btnLogOut = findViewById<Button>(R.id.btnLogout)
 
         btnLogOut.setOnClickListener{ view ->
@@ -37,6 +63,8 @@ class Timeline : AppCompatActivity() {
     fun showMessage(view: View, message: String){
         Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE).setAction("Action", null).show()
     }
+
+
 
 }
 
